5ae26b16291ac6afbb8c204be0559c8834d0fcd6
dungdv95 <dungdv95.bg@gmail.com>
2023-12-19 22:04:03 +0700

    update

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -4,7 +4,17 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { ChevronDownIcon, Mail, Phone } from "lucide-react";
+import {
+  ChevronDownIcon,
+  Facebook,
+  Instagram,
+  Mail,
+  Phone,
+  Search,
+  Twitter,
+  Youtube,
+} from "lucide-react";
+import Image from "next/image";
 
 const user = {
   name: "Tom Cook",
@@ -13,15 +23,11 @@ const user = {
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-  { name: "Projects", href: "#", current: false },
-  { name: "Calendar", href: "#", current: false },
-];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
+  { name: "Trang trủ", href: "#", current: true },
+  { name: "Giới thiệu", href: "#", current: false },
+  { name: "Dịch vụ", href: "#", current: false },
+  { name: "Tin tức", href: "#", current: false },
+  { name: "Liên hệ", href: "#", current: false },
 ];
 
 function classNames(...classes: any) {
@@ -46,51 +52,55 @@ export function Sidebar() {
                     <span className="text-sm">Email:demo@gmail.com</span>
                   </div>
                   <Languages />
-                  <div>c</div>
+                  <div className="flex gap-4 items-center">
+                    <Facebook className="h-5 w-5 fill-gray-800 stroke-none" />
+                    <Instagram className="h-5 w-5" />
+                    <Twitter className="h-5 w-5 fill-gray-800 stroke-none" />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      strokeWidth={2}
+                    >
+                      <path d="M 12 4 C 12 4 5.7455469 3.9999687 4.1855469 4.4179688 C 3.3245469 4.6479688 2.6479687 5.3255469 2.4179688 6.1855469 C 1.9999687 7.7455469 2 12 2 12 C 2 12 1.9999687 16.254453 2.4179688 17.814453 C 2.6479687 18.675453 3.3255469 19.352031 4.1855469 19.582031 C 5.7455469 20.000031 12 20 12 20 C 12 20 18.254453 20.000031 19.814453 19.582031 C 20.674453 19.352031 21.352031 18.674453 21.582031 17.814453 C 22.000031 16.254453 22 12 22 12 C 22 12 22.000031 7.7455469 21.582031 6.1855469 C 21.352031 5.3255469 20.674453 4.6479688 19.814453 4.4179688 C 18.254453 3.9999687 12 4 12 4 z M 12 6 C 14.882 6 18.490875 6.1336094 19.296875 6.3496094 C 19.465875 6.3946094 19.604391 6.533125 19.650391 6.703125 C 19.891391 7.601125 20 10.342 20 12 C 20 13.658 19.891391 16.397875 19.650391 17.296875 C 19.605391 17.465875 19.466875 17.604391 19.296875 17.650391 C 18.491875 17.866391 14.882 18 12 18 C 9.119 18 5.510125 17.866391 4.703125 17.650391 C 4.534125 17.605391 4.3956094 17.466875 4.3496094 17.296875 C 4.1086094 16.398875 4 13.658 4 12 C 4 10.342 4.1086094 7.6011719 4.3496094 6.7011719 C 4.3946094 6.5331719 4.533125 6.3946094 4.703125 6.3496094 C 5.508125 6.1336094 9.118 6 12 6 z M 10 8.5351562 L 10 15.464844 L 16 12 L 10 8.5351562 z"></path>
+                    </svg>
+                  </div>
                 </div>
               </div>
-              <nav
-                className=" lg:flex lg:space-x-8 lg:py-2"
-                aria-label="Global"
-              >
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className={classNames(
-                      item.current
-                        ? "bg-gray-100 text-gray-900"
-                        : "text-gray-900 hover:bg-gray-50 hover:text-gray-900",
-                      "inline-flex items-center rounded-md py-2 px-3 text-sm font-medium"
-                    )}
-                    aria-current={item.current ? "page" : undefined}
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </nav>
+              <div className="flex justify-between py-2">
+                <div className="flex items-center">
+                  <Image
+                    src="/asset/logo.png"
+                    alt=""
+                    width={38}
+                    height={38}
+                    style={{ width: "100%", height: "auto" }}
+                  />
+                </div>
+                <div className="flex items-center gap-3">
+                  <nav className="flex gap-5 items-center" aria-label="Global">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-900 hover:bg-gray-50 hover:text-gray-900",
+                          "inline-flex items-center rounded-md py-1 px-3 text-sm font-medium"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </nav>
+                  <Search className="h-4 w-4" />
+                </div>
+              </div>
             </div>
           </div>
-          {/* <div className="divide-y divide-gray-200">
-            <div className="relative flex h-16 justify-between">abcd</div>
-            <nav className=" lg:flex lg:space-x-8 lg:py-2" aria-label="Global">
-              {navigation.map((item) => (
-                <Link
-                  key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-100 text-gray-900"
-                      : "text-gray-900 hover:bg-gray-50 hover:text-gray-900",
-                    "inline-flex items-center rounded-md py-2 px-3 text-sm font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </Link>
-              ))}
-            </nav>
-          </div> */}
         </>
       )}
     </Disclosure>
